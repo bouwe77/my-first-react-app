53bcffe263412f49522b480ba5ddeb648d11361c
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2018-04-12 12:24:22 +0200

    .

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import { render } from 'react-dom';
-import Hello from './Hello';
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import React from "react";
+import { render } from "react-dom";
+import Hello from "./Hello";
+//import { Provider } from 'react-redux'
+//import { createStore } from 'redux'
 
 const App = () => (
   <div>
     <Hello name="CodeSandbox" />
-    <h2>Start editing to see some magic happen {'\u2728'}</h2>
+    <h2>Start editing to see some magic happen {"\u2728"}</h2>
   </div>
 );
 
-render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById("root"));
